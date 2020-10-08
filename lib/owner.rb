8eac65d981dbe1502613b7e979d1fe660834719a
warrenniu <warrenniu@gmail.com>
2020-10-08 14:15:03 -0400

    In progress. 9 failures left

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,21 +1,25 @@
 class Owner
   
-  attr_accessor :name
+  attr_reader :name, :species
 
   @@all = []
 
   def initialize(name)
     @name = name
     @@all << self
+    @species = "human"
   end
 
   def name
+    @name
   end
 
   def species
+    @species
   end
 
   def say_species
+    "I am a #{self.species}."
   end
 
   def self.all
@@ -27,6 +31,33 @@ class Owner
   end
 
   def self.reset_all
+    self.all.clear
+  end
+
+  def cats
+    Cat.all.select {|cat| cat.owner == self}
+  end
+
+  def dogs
+    Dog.all.select {|dog| dog.owner == self}
+  end
+
+  def buy_cat
+  end
+  
+  def buy_dog
+  end
+
+  def walk_dogs
+  end
+
+  def feed_cats
+  end
+
+  def sell_pets
+  end
+
+  def list_pets
   end
 
 
