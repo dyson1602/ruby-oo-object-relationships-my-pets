90a1b613dd00c1009f9b1a871712eeeea90ff98c
dyson1602 <cclark000@citymail.cuny.edu>
2020-10-08 13:50:27 -0400

    first commit with 20 errors remaining

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,35 @@
 class Owner
-  # code goes here
+  
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def name
+  end
+
+  def species
+  end
+
+  def say_species
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    self.all.count
+  end
+
+  def self.reset_all
+  end
+
+
+
+
 end
