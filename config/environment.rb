86b140381dcd5f5fc0476c77c50da048a1c64751
warrenniu <warrenniu@gmail.com>
2020-10-08 13:14:58 -0400

    Edited

--- config/environment.rb
+++ config/environment.rb
@@ -2,3 +2,4 @@ Dir[File.join(File.dirname(__FILE__), "../lib", "*.rb")].each {|f| require f}
 
 
 #This is the environment, and I am editing it. This is Chris's edit.
+#That was a dope comment - Warren
